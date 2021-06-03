6beab8613d54f0158e44e747e758a44e6bc82190
GiorunoGiovanna <971615621@qq.com>
2021-06-03 21:50:16 +0800

    Support multi-input-topics config

--- front-end/src/api/func.js
+++ front-end/src/api/func.js
@@ -1,83 +1,49 @@
 import request from '@/utils/request'
 
-const {
-    post,
-    get
-} = request
+const { post, get } = request;
 
 export const funcApi = {
-    list: '/function/list',
-    create: '/function/create',
-    info: ({
-        funcName
-    }) => `/function/${funcName}/info`,
-    addFunc: ({
-        funcName
-    }) => `/function/${funcName}/add`,
-    stats: ({
-        funcName
-    }) => `/function/${funcName}/stats`,
-    status: ({
-        funcName
-    }) => `/function/${funcName}/status`,
-    trigger: ({
-        funcName
-    }) => `/function/${funcName}/trigger`,
-    deleteFunc: ({
-        funcName
-    }) => `/function/${funcName}/delete`
+  list: '/function/list',
+  create: '/function/create',
+  info: ({ funcName }) => `/function/${funcName}/info`,
+  stats: ({ funcName }) => `/function/${funcName}/stats`,
+  status: ({ funcName }) => `/function/${funcName}/status`,
+  trigger: ({ funcName }) => `/function/${funcName}/trigger`,
+  deleteFunc: ({ funcName }) => `/function/${funcName}/delete`,
+  addFunc: ({ funcName }) => `/function/${funcName}/add`,
 }
 
-export function getList () {
-    return get(funcApi.list)
+export function getList() {
+  return get(funcApi.list);
 }
 
-export function createFunc (data) {
-    return post(funcApi.create, {
-        data
-    })
+export function createFunc(data) {
+  return post(funcApi.create, { data });
 }
 
-export function getInfo (funcName) {
-    return get(funcApi.info({
-        funcName
-    }))
+export function getInfo(funcName) {
+  return get(funcApi.info({ funcName }));
 }
 
-// export function getTest(funcName) {
-//   return get(funcApi.test({ funcName }));
-// }
-
-export function getStats (funcName) {
-    return get(funcApi.stats({
-        funcName
-    }))
+export function getStats(funcName) {
+  return get(funcApi.stats({ funcName }));
 }
 
-export function getStatus (funcName) {
-    return get(funcApi.status({
-        funcName
-    }))
+export function getStatus(funcName) {
+  return get(funcApi.status({ funcName }));
+}
+
+export function triggerFunc(funcName, data) {
+  return post(funcApi.trigger({ funcName }), { data });
+}
+
+export function deleteFunc(funcName) {
+  return post(funcApi.deleteFunc({ funcName }));
 }
 
 export function addFunc (funcName, data) {
-    return post(funcApi.addFunc({
-        funcName
-    }), {
-        data
-    })
+    return post(funcApi.addFunc({ funcName }), { data })
 }
 
-export function triggerFunc (funcName, data) {
-    return post(funcApi.trigger({
-        funcName
-    }), {
-        data
-    })
-}
 
-export function deleteFunc (funcName) {
-    return post(funcApi.deleteFunc({
-        funcName
-    }))
-}
+
