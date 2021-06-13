d63c7bbdd84b61af8ae414dac2e20e18d0b49d6a
GiorunoGiovanna <971615621@qq.com>
2021-06-13 10:40:03 +0800

    update inputs

--- front-end/src/views/images/components/AddForm.vue
+++ front-end/src/views/images/components/AddForm.vue
@@ -1,213 +1,64 @@
 <template>
-  <a-drawer :width="720" :visible="visible" :body-style="{ paddingBottom: '40px' }" @close="onClose">
-    <a-form :form="form" layout="vertical" hide-required-mark>
-      <a-row>
-        <a-form-item label="Inputs" v-decorator="">
-          <a-input name='inputs' v-decorator="[
-                  'inputs',
-                  {
-                    rules: [{ required: true, message: 'Please enter inputs', },
-                    ],
-                  },
-                ]" placeholder="Please enter inputs" allowClear @pressEnter="enterInput" />
-        </a-form-item>
-        <a-breadcrumb>
-          <a-breadcrumb-item v-for="(value,index) in input"><a @click="editInput(index)">{{value}}</a>
-            <a-icon type="close-circle" @click='deleteInputs(index)' />
-          </a-breadcrumb-item>
-        </a-breadcrumb>
-      </a-row>
-      <a-row>
-        <a-form-item label="Output">
-          <a-input v-decorator="[
-                  'output',
-                  {
-                    rules: [{ required: true, message: 'Please enter output' }],
-                    initialValue:'persistent://public/default/output-topic',
-                  },
-                ]" placeholder="Please enter output" allowClear />
-        </a-form-item>
-      </a-row>
-      <a-row>
-        <a-form-item label="Log-topic">
-          <a-input v-decorator="[
-                  'log-topic',
-                  {
-                    rules: [{ required: true, message: 'Please enter log-topic' }],
-                    initialValue:'persistent://public/default/log-topic',
-                  },
-                ]" placeholder="Please enter log-topic" allowClear />
-        </a-form-item>
-      </a-row>
-      <a-row :gutter="16">
-        <a-col :span="12">
-          <a-form-item label="ClassName">
-            <a-input v-decorator="[
-                  'className',
-                  {
-                    rules: [{ required: true, message: 'Please enter className' }],
-                  },
-                ]" placeholder="Please enter className" allowClear />
-          </a-form-item>
-        </a-col>
-        <a-col :span="12">
-          <a-form-item label="Runtime">
-            <a-select v-decorator="[
-                    'runtime',
-                    { 
-                      rules: [{ required: true }],
-                      initialValue:'JAVA'
-                    },
-                  ]" @change="onRuntimeChg">
-              <a-select-option value="JAVA">
-                JAVA
-              </a-select-option>
-              <a-select-option value="GO">
-                GO
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-        </a-col>
-      </a-row>
-
-      <a-form-item label="Dragger">
-        <div class="dropbox">
-          <a-upload-dragger v-decorator="[
-                                'data',
-                                {
-                                  rules: [{ required: true, message: 'Please dragger function file' }],
-                                  valuePropName: 'fileList',
-                                  getValueFromEvent: normFile,
-                                },
-                              ]" :before-upload="fbeforeUpload" name="data">
-            <p class="ant-upload-drag-icon">
-              <a-icon type="inbox" />
-            </p>
-            <p class="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p class="ant-upload-hint">
-              Only jar files are supported
-            </p>
-          </a-upload-dragger>
-        </div>
-      </a-form-item>
-    </a-form>
-    <div :style="{
-          position: 'absolute',
-          right: 0,
-          bottom: 0,
-          width: '100%',
-          borderTop: '1px solid #e9e9e9',
-          padding: '10px 16px',
-          background: '#fff',
-          textAlign: 'right',
-          zIndex: 1,
-        }">
-      <a-button :style="{ marginRight: '16px' }" @click="onReset">
-        reset
-      </a-button>
-      <a-button type="primary" @click="onSub">
-        Add Function
-      </a-button>
-    </div>
-  </a-drawer>
+  <div class="clearfix">
+    <a-upload :file-list="fileList" :remove="handleRemove" :before-upload="beforeUpload">
+      <a-button> <a-icon type="upload" /> Select File </a-button>
+    </a-upload>
+    <a-button
+      type="primary"
+      :disabled="fileList.length === 0"
+      :loading="uploading"
+      style="margin-top: 16px"
+      @click="handleUpload"
+    >
+      {{ uploading ? 'Uploading' : 'Start Upload' }}
+    </a-button>
+  </div>
 </template>
-
 <script>
-  import { addFunc } from '@/api/func'
-  export default {
-    data() {
-      return {
-        form: this.$form.createForm(this),
-        input: [
-          "persistent://public/default/iutput-topic",
-          "persistent://public/default/iutput-topic1"
-        ],
-        isEdit: -1
-      };
+
+export default {
+  data () {
+    return {
+      fileList: [],
+      uploading: false
+    }
+  },
+  methods: {
+    handleRemove (file) {
+      const index = this.fileList.indexOf(file)
+      const newFileList = this.fileList.slice()
+      newFileList.splice(index, 1)
+      this.fileList = newFileList
     },
-    props: {
-      visible: {
-        type: Boolean,
-        default: false,
-      }
+    beforeUpload (file) {
+      this.fileList = [...this.fileList, file]
+      return false
     },
-    methods: {
-      showDrawer() {
-        this.$parent.showDrawer();
-      },
-      onClose() {
-        this.$parent.closeDrawer();
-      },
-      onReset() {
-        console.log('reset')
-        this.form.resetFields();
-      },
-      onSub() {
-        this.form.validateFields((err, values) => {
-          if (err) return
-          // console.log('Received values of form: ', values)
-          values.inputs = this.input
-          // console.log(values)
-          addFunc(values.functionName, values)
-            .then((res) => {
-              console.log("success!")
-            })
-        })
-      },
-      onRuntimeChg(value) {
-        this.form.setFieldsValue({
-          runtime: value,
-        });
-      },
-      normFile(e) {
-        // console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-          return [e[e.length - 1]];
-        }
-        if (e && e.fileList.length > 0) return [e.fileList[e.fileList.length - 1]];
-        return [];
-      },
-      fbeforeUpload() {
-        return false;
-      },
-      enterInput(e) { //输入input(支持逐个输入)
-        const value = e.target.value;
-        var pattern = /\ /;
-        // console.log(pattern.test(value));
-        if (!value) {
-          this.$message.error("不能为空!");
-          return
-        } else if (pattern.test(value)) {
-          this.$message.error("不能含有空格！！");
-          return
-        }
-        for (var i = 0; i < this.input.length; i++) {
-          if (value == this.input[i]) {
-            this.$message.error("已有该input！");
-            return
-          }
-        }
-        if (this.isEdit != -1) {
-          this.input.splice(this.isEdit, 1, value);
-          // this.form.resetFields('inputs');
-        } else {
-          this.input.push(value)
-          // this.form.resetFields('inputs');
-        }
-        this.isEdit = -1
-      },
-      deleteInputs(index) {
-        this.input.splice(index, 1);
-      },
-      editInput(index) {
-        this.form.setFieldsValue({
-          inputs: this.input[index]
-        })
-        this.isEdit = index
-        // console.log(this.form)
-      }
-    },
+    handleUpload () {
+      const { fileList } = this
+      const formData = new FormData()
+      fileList.forEach(file => {
+        formData.append('files[]', file)
+      })
+      this.uploading = true
+
+      // You can use any AJAX library you like
+      // reqwest({
+      //   url: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+      //   method: 'post',
+      //   processData: false,
+      //   data: formData,
+      //   success: () => {
+      //     this.fileList = [];
+      //     this.uploading = false;
+      //     this.$message.success('upload successfully.');
+      //   },
+      //   error: () => {
+      //     this.uploading = false;
+      //     this.$message.error('upload failed.');
+      //   },
+      // });
+    }
   }
+}
 </script>
