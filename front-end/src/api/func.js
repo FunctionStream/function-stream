bbbab0bb88044d0ee0ed65dd147bcafd033253f2
root <yxqi09@126.com>
2021-06-13 13:56:00 +0800

    API joint for pulsar backend: Get List of function, Get function info, Get function stats, Get function status

--- front-end/src/api/func.js
+++ front-end/src/api/func.js
@@ -1,44 +1,41 @@
 import request from '@/utils/request'
 
-const { post, get } = request;
+const { post, get } = request
 
 export const funcApi = {
   list: '/admin/v3/functions/public/default',
-  create: 'admin/v3/functions/public/default/myfunc',
+  create: '/admin/v3/functions/public/default/myfunc',
   info: ({ funcName }) => `/admin/v3/functions/public/default/${funcName}`,
   stats: ({ funcName }) => `/admin/v3/functions/public/default/${funcName}/stats`,
   status: ({ funcName }) => `/admin/v3/functions/public/default/${funcName}/status`,
-  trigger: ({ funcName }) => `/functions/${funcName}/trigger`,
-  deleteFunc: ({ funcName }) => `/functions/${funcName}/delete`,
+  trigger: ({ funcName }) => `/admin/v3/functions/public/default/${funcName}/trigger`,
+  deleteFunc: ({ funcName }) => `/admin/v3/functions/public/default/${funcName}`
 }
 
-export function getList() {
-  return get(funcApi.list);
+export function getList () {
+  return get(funcApi.list)
 }
 
-export function createFunc(data) {
-  return post(funcApi.create, { data });
+export function createFunc (data) {
+  return post(funcApi.create, { data })
 }
 
-export function getInfo(funcName) {
-  return get(funcApi.info({ funcName }));
+export function getInfo (funcName) {
+  return get(funcApi.info({ funcName }))
 }
 
-export function getStats(funcName) {
-  return get(funcApi.stats({ funcName }));
+export function getStats (funcName) {
+  return get(funcApi.stats({ funcName }))
 }
 
-export function getStatus(funcName) {
-  return get(funcApi.status({ funcName }));
+export function getStatus (funcName) {
+  return get(funcApi.status({ funcName }))
 }
 
-export function triggerFunc(funcName, data) {
-  return post(funcApi.trigger({ funcName }), { data });
+export function triggerFunc (funcName, data) {
+  return post(funcApi.trigger({ funcName }), { data })
 }
 
-export function deleteFunc(funcName) {
-  return post(funcApi.deleteFunc({ funcName }));
+export function deleteFunc (funcName) {
+  return post(funcApi.deleteFunc({ funcName }))
 }
-
-
-
