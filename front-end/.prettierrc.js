d13192c32c5dfe5aabc4a229650a7a3af3b41f6d
yeliulee <yeliulee@qq.com>
2021-06-27 21:36:43 +0800

    feat(config): initialize prettier config

---
+++ front-end/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  tabWidth: 2,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'none',
+  vueIndentScriptAndStyle: true
+}
