e6c600a30c5695cad15a41880b7c6dd57a653b50
winnd <AS40879@yeah.net>
2021-08-07 15:57:12 +0800

    fix: fix the left side menu layout problem

--- front-end/src/components/Menu.vue
+++ front-end/src/components/Menu.vue
@@ -1,20 +1,18 @@
 <template>
-  <div>
-    <component
-      :is="item.children ? 'ElSubmenu' : 'ElMenuItem'"
-      v-for="item in menus"
-      :key="item.path"
-      :index="item.path"
-      :route="item"
-    >
-      <i v-if="!item.children" :class="item.meta.icon"></i>
-      <template #title>
-        <i v-if="item.children" :class="item.meta.icon"></i>
-        <span>{{ $t(item.name) }}</span>
-      </template>
-      <SubMenu :menus="item.children" />
-    </component>
-  </div>
+  <component
+    :is="item.children ? 'ElSubmenu' : 'ElMenuItem'"
+    v-for="item in menus"
+    :key="item.path"
+    :index="item.path"
+    :route="item"
+  >
+    <i v-if="!item.children" :class="item.meta.icon"></i>
+    <template #title>
+      <i v-if="item.children" :class="item.meta.icon"></i>
+      <span>{{ $t(item.name) }}</span>
+    </template>
+    <SubMenu :menus="item.children" />
+  </component>
 </template>
 
 <script>
