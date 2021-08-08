38b01488d3f67cfc6be0d58166577a4afa853cea
youcike1997 <1935722141@qq.com>
2021-08-08 23:15:37 +0800

    reformat codes

--- back-end/token/src/main/java/org/functionstream/functions/token/GetTokenFunction.java
+++ back-end/token/src/main/java/org/functionstream/functions/token/GetTokenFunction.java
@@ -1,25 +1,26 @@
 package org.functionstream.functions.token;
 
-import com.google.gson.*;
-
 import javax.crypto.spec.SecretKeySpec;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.jsonwebtoken.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.pulsar.functions.api.Context;
 import org.apache.pulsar.functions.api.Function;
 
-public class GetTokenFunction implements Function<String,String> {
+public class GetTokenFunction implements Function<String, String> {
 
-    private static String secret="21232f297a57a5a743894a0e4a801fc3";
-
-    private static SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
-    private static long SurvivalTime=60*60*1000;
+    private static String SECRET = "21232f297a57a5a743894a0e4a801fc3";
+    private static SignatureAlgorithm SIGNATUREALGORITHM = SignatureAlgorithm.HS256;
+    private static long SURVIVALTIME = 60 * 60 * 1000;
 
     @Override
     public String process(String input, Context context) throws Exception {
@@ -30,25 +31,25 @@ public class GetTokenFunction implements Function<String,String> {
         String password = jsonObject.get("password").getAsString();
 
         //Verify username and password from the database
-        if(TokenJDBC.checkAccount(userName,password)){
+        if (TokenJDBC.checkAccount(userName, password)) {
             jsonObject.remove("userName");
             jsonObject.remove("password");
             String token = createToken(userName);
-            jsonObject.add("token",new Gson().toJsonTree(token));
-        }else {
+            jsonObject.add("token", new Gson().toJsonTree(token));
+        } else {
             throw new Exception("GetTokenFunction:incorrect username or password");
         }
         return jsonObject.toString();
     }
 
-    private static String createToken(String userName){
-        Key signingKey = new SecretKeySpec(secret.getBytes(), signatureAlgorithm.getJcaName());
+    private static String createToken(String userName) {
+        Key signingKey = new SecretKeySpec(SECRET.getBytes(), SIGNATUREALGORITHM.getJcaName());
         Map<String, Object> claims = new HashMap<>();
         claims.put("role", userName);
         JwtBuilder builder = Jwts.builder().setClaims(claims)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+SurvivalTime))
-                .signWith(signatureAlgorithm,signingKey);
+                .setExpiration(new Date(System.currentTimeMillis() + SURVIVALTIME))
+                .signWith(SIGNATUREALGORITHM, signingKey);
 
         return builder.compact();
     }
--- back-end/token/src/main/java/org/functionstream/functions/token/TokenJDBC.java
+++ back-end/token/src/main/java/org/functionstream/functions/token/TokenJDBC.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 public class TokenJDBC {
 
     private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
-    private static final String url = "jdbc:mysql://localhost:3306/test?characterEncoding=utf8&useSSL=false&serverTimezone=CST";
-    private static final String username = "root";
-    private static final String password = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/test?characterEncoding=utf8&useSSL=false&serverTimezone=CST";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     public static boolean checkAccount(String userIn, String passwordIn) throws Exception {
         Connection connection = null;
@@ -19,23 +19,23 @@ public class TokenJDBC {
         ResultSet resultSet = null;
         try {
             Class.forName(DRIVER_CLASS);
-            connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             String sql = "select * from account where username = ? and password = ?";
             preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1,userIn);
-            preparedStatement.setString(2,passwordIn);
+            preparedStatement.setString(1, userIn);
+            preparedStatement.setString(2, passwordIn);
             resultSet = preparedStatement.executeQuery();
             return resultSet.next();
         } catch (ClassNotFoundException | SQLException e) {
             throw new Exception("GetTokenFunction:SQL Exception");
         } finally {
-            if(null != resultSet){
+            if (null != resultSet) {
                 resultSet.close();
             }
-            if(null != preparedStatement){
+            if (null != preparedStatement) {
                 preparedStatement.close();
             }
-            if(null != connection){
+            if (null != connection) {
                 connection.close();
             }
         }
