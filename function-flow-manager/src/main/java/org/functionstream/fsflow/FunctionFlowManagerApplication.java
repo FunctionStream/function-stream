ee9b0a141a23df4712350ae2da6fbad1915eae41
Zike Yang <ar@armail.top>
2021-10-06 17:22:16 +0800

    Init function stream flow manager.
    Signed-off-by: Zike Yang <ar@armail.top>

---
+++ function-flow-manager/src/main/java/org/functionstream/fsflow/FunctionFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package org.functionstream.fsflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionFlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionFlowManagerApplication.class, args);
+	}
+
+}
---
+++ function-flow-manager/src/test/java/org/functionstream/fsflow/FunctionFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.functionstream.fsflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionFlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
