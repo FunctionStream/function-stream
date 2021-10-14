eae5e2def203b41c9302c9871acf0765df66734e
Akhil-Yuan <yxqi09@126.com>
2021-10-14 12:22:45 +0800

    Add dock menu demo

---
+++ front-end/src/views/function-flow/components/TopicControl.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <text>test-setting</text>
+    <input
+      type="topic"
+      :readonly="readonly"
+      :value="value"
+      @input="change($event)"
+      @dblclick.stop=""
+      @pointerdown.stop=""
+      @pointermove.stop=""
+    />
+  </div>
+</template>
+
+<script>
+  import { ref } from '@vue/reactivity'
+  import { onMounted } from '@vue/runtime-core'
+  export default {
+    props: {
+      emitter: {
+        type: Object,
+        default: null
+      },
+      ikey: {
+        type: String,
+        default: null
+      },
+      getData: {
+        type: Function,
+        default: () => {}
+      },
+      putData: {
+        type: Function,
+        default: () => {}
+      },
+      readonly: Boolean
+    },
+    setup(props) {
+      const value = ref('')
+      onMounted(() => {
+        props.putData('some-setting', value.value)
+        value.value = props.getData(props.ikey)
+      })
+
+      function change(e) {
+        value.value = e.target.value
+        props.putData(props.ikey, value.value)
+      }
+    }
+  }
+</script>
