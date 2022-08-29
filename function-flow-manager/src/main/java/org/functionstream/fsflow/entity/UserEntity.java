28333dce7b5cec05e01b756efd2393594a161fb2
CarrotSaltDog <lazyfish0176@gamil.com>
2022-08-29 14:18:43 +0800

    Changes

---
+++ function-flow-manager/src/main/java/org/functionstream/fsflow/entity/UserEntity.java
@@ -0,0 +1,13 @@
+package org.functionstream.fsflow.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    private String username;
+    private String password;
+}
