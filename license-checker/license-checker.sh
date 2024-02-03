f7105d90a93e27b52f18e45d2c9e6a1d117cdfbf
Zike Yang <zike@apache.org>
2024-02-03 19:36:18 +0800

    build: add license checker (#7)

---
+++ license-checker/license-checker.sh
@@ -0,0 +1,34 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xe
+
+cd "$(git rev-parse --show-toplevel)"
+
+LICENSE_CHECKER="bin/license-header-checker"
+
+if [ ! -f "$LICENSE_CHECKER" ]; then
+  echo "license-checker not found, building it..."
+  export BINDIR=bin && curl -s https://raw.githubusercontent.com/lluissm/license-header-checker/master/install.sh | bash
+fi
+
+$LICENSE_CHECKER -a -r -i bin,restclient,common/run.go,common/signal.go ./license-checker/license-header.txt . go
+$LICENSE_CHECKER -a -r -i bin,restclient ./license-checker/license-header-sh.txt . sh yaml yml
+$LICENSE_CHECKER -a -r -i bin,restclient ./license-checker/license-header-md.txt . md
+
+if [[ -z $(git status -s) ]]; then
+  echo "No license header issues found"
+else
+  echo "$(git status)"
+  echo "License header issues found"
+  exit 1
+fi
