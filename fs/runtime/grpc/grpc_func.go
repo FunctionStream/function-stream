52d62a711975046fe2a1ad0f35ff9ecfe727c296
Zike Yang <zike@apache.org>
2024-02-25 19:57:29 +0800

    fix: fix race issue in tests
    Signed-off-by: Zike Yang <zike@apache.org>

--- fs/runtime/grpc/grpc_func.go
+++ fs/runtime/grpc/grpc_func.go
@@ -90,11 +90,15 @@ func (s *FSSReconcileServer) Reconcile(req *proto.ConnectRequest, stream proto.F
 		atomic.StoreInt32(&s.status, int32(NotReady))
 		s.log.InfoContext(s.ctx, "grpc reconcile stream closed")
 	}()
-	for _, v := range s.functions {
-		err := stream.Send(v.status)
-		if err != nil {
-			s.log.ErrorContext(stream.Context(), "failed to send status update", slog.Any("status", v.status))
-			// Continue to send the next status update.
+	{
+		s.functionsMu.Lock()
+		defer s.functionsMu.Unlock()
+		for _, v := range s.functions {
+			err := stream.Send(v.status)
+			if err != nil {
+				s.log.ErrorContext(stream.Context(), "failed to send status update", slog.Any("status", v.status))
+				// Continue to send the next status update.
+			}
 		}
 	}
 	for {
