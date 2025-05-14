15e402d479ea9f690ca910beb60f475ecc5854aa
Zike Yang <zike@apache.org>
2025-05-14 20:59:17 +0800

    Fix test warning
    Signed-off-by: Zike Yang <zike@apache.org>

--- sdks/fs-python/tests/conftest.py
+++ sdks/fs-python/tests/conftest.py
@@ -6,13 +6,6 @@ import pytest
 import asyncio
 from unittest.mock import Mock
 
-@pytest.fixture(scope="session")
-def event_loop():
-    """Create an instance of the default event loop for each test case."""
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
 @pytest.fixture
 def mock_pulsar_message():
     """Create a mock Pulsar message."""
