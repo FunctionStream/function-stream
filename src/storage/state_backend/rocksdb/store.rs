368939fdd4b29103cfbe02025285bc310c3a69d4
luoluoyuyu <zhenyu@apache.org>
2026-02-03 23:11:46 +0800

    update code

--- src/storage/state_backend/rocksdb/store.rs
+++ src/storage/state_backend/rocksdb/store.rs
@@ -13,18 +13,16 @@
 use crate::storage::state_backend::error::BackendError;
 use crate::storage::state_backend::key_builder::{build_key, increment_key, is_all_0xff};
 use crate::storage::state_backend::store::{StateIterator, StateStore};
-use rocksdb::{DB, IteratorMode, WriteBatch};
-use std::sync::{Arc, Mutex};
+use rocksdb::{
+    BlockBasedOptions, Cache, ColumnFamilyDescriptor, DB, DBCompressionType, Direction,
+    IteratorMode, Options, ReadOptions, WriteBatch,
+};
+use std::path::Path;
+use std::sync::Arc;
 
-/// RocksDB state store
 pub struct RocksDBStateStore {
-    /// RocksDB database instance
     db: Arc<DB>,
-    /// Column family name (if using column family)
-    ///
-    /// In MultiThreaded mode, get handle by name for each operation
-    /// This avoids lifetime and pointer issues
-    column_family_name: Option<String>,
+    cf_name: String,
 }
 
 impl RocksDBStateStore {
@@ -37,197 +35,97 @@ impl RocksDBStateStore {
     /// # Returns
     /// - `Ok(Box<dyn StateStore>)`: successfully created
     /// - `Err(BackendError)`: creation failed
-    ///
-    /// Note: Caller should ensure the column family already exists (factory will create it automatically)
     pub fn new_with_factory(
         db: Arc<DB>,
         column_family: Option<String>,
     ) -> Result<Box<dyn StateStore>, BackendError> {
-        let cf_name = if let Some(ref cf_name) = column_family {
-            if cf_name != "default" {
-                if db.cf_handle(cf_name).is_none() {
-                    return Err(BackendError::Other(format!(
-                        "Column family '{}' does not exist. This should not happen as factory should create it.",
-                        cf_name
-                    )));
-                }
-                Some(cf_name.clone())
-            } else {
-                None
-            }
-        } else {
-            None
-        };
-
-        Ok(Box::new(Self {
-            db,
-            column_family_name: cf_name,
-        }))
+        let cf_name = column_family.unwrap_or_else(|| "default".to_string());
+        if db.cf_handle(&cf_name).is_none() {
+            return Err(BackendError::Other(format!(
+                "Column family '{}' does not exist",
+                cf_name
+            )));
+        }
+        Ok(Box::new(Self { db, cf_name }))
     }
 
-    /// Open RocksDB store (static method, simplified version)
-    ///
-    /// # Arguments
-    /// - `name`: database path
-    ///
-    /// # Returns
-    /// - `Ok(Box<dyn StateStore>)`: successfully opened
-    /// - `Err(BackendError)`: open failed
-    pub fn open<P: AsRef<std::path::Path>>(name: P) -> Result<Box<dyn StateStore>, BackendError> {
-        let path = name.as_ref();
-
-        let mut opts = rocksdb::Options::default();
+    pub fn open<P: AsRef<Path>>(path: P, cf_name: Option<String>) -> Result<Self, BackendError> {
+        let mut opts = Options::default();
         opts.create_if_missing(true);
+        opts.create_missing_column_families(true);
         opts.set_merge_operator_associative("appendOp", merge_operator);
+        opts.set_compression_type(DBCompressionType::Lz4);
 
-        let db = DB::open(&opts, path)
-            .map_err(|e| BackendError::IoError(format!("Failed to open RocksDB: {}", e)))?;
+        let mut block_opts = BlockBasedOptions::default();
+        block_opts.set_block_size(16 * 1024);
+        block_opts.set_cache_index_and_filter_blocks(true);
+        block_opts.set_block_cache(&Cache::new_lru_cache(128 * 1024 * 1024));
+        opts.set_block_based_table_factory(&block_opts);
 
-        Self::new_with_factory(Arc::new(db), None)
+        let target_cf = cf_name.unwrap_or_else(|| "default".to_string());
+
+        let cf_descriptors = vec![
+            ColumnFamilyDescriptor::new("default", Options::default()),
+            ColumnFamilyDescriptor::new(&target_cf, opts.clone()),
+        ];
+
+        let db = DB::open_cf_descriptors(&opts, path, cf_descriptors)
+            .map_err(|e| BackendError::IoError(e.to_string()))?;
+
+        Ok(Self {
+            db: Arc::new(db),
+            cf_name: target_cf,
+        })
     }
 
-    /// Helper method for operations using column family
-    fn put_cf(&self, key: &[u8], value: &[u8]) -> Result<(), BackendError> {
-        if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            self.db.put_cf(&cf, key, value)
-        } else {
-            self.db.put(key, value)
-        }
-        .map_err(|e| BackendError::IoError(format!("RocksDB put error: {}", e)))?;
-        Ok(())
-    }
-
-    fn get_cf(&self, key: &[u8]) -> Result<Option<Vec<u8>>, BackendError> {
-        let result = if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            self.db.get_cf(&cf, key)
-        } else {
-            self.db.get(key)
-        }
-        .map_err(|e| BackendError::IoError(format!("RocksDB get error: {}", e)))?;
-
-        Ok(result)
-    }
-
-    fn delete_cf(&self, key: &[u8]) -> Result<(), BackendError> {
-        if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            self.db.delete_cf(&cf, key)
-        } else {
-            self.db.delete(key)
-        }
-        .map_err(|e| BackendError::IoError(format!("RocksDB delete error: {}", e)))?;
-        Ok(())
-    }
-
-    fn merge_cf(&self, key: &[u8], value: &[u8]) -> Result<(), BackendError> {
-        if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            self.db.merge_cf(&cf, key, value)
-        } else {
-            self.db.merge(key, value)
-        }
-        .map_err(|e| BackendError::IoError(format!("RocksDB merge error: {}", e)))?;
-        Ok(())
-    }
-
-    fn delete_range_cf(&self, start: &[u8], end: &[u8]) -> Result<(), BackendError> {
-        let mut batch = WriteBatch::default();
-
-        if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            batch.delete_range_cf(&cf, start, end);
-        } else {
-            batch.delete_range(start, end);
-        }
-
+    #[inline(always)]
+    fn cf_handle(&self) -> Result<Arc<rocksdb::BoundColumnFamily<'_>>, BackendError> {
         self.db
-            .write(batch)
-            .map_err(|e| BackendError::IoError(format!("RocksDB delete_range error: {}", e)))?;
-        Ok(())
+            .cf_handle(&self.cf_name)
+            .ok_or_else(|| BackendError::Other(format!("Handle for CF '{}' invalid", self.cf_name)))
     }
-}
-
-/// Merge operator: append operation
-fn merge_operator(
-    _new_key: &[u8],
-    existing_val: Option<&[u8]>,
-    operands: &rocksdb::MergeOperands,
-) -> Option<Vec<u8>> {
-    use std::io::Write;
-
-    let mut buf = Vec::new();
-
-    for operand in operands {
-        buf.write_all(operand).ok()?;
-    }
-
-    if let Some(existing) = existing_val {
-        buf.write_all(existing).ok()?;
-    }
-
-    Some(buf)
 }
 
 impl StateStore for RocksDBStateStore {
     fn put_state(&self, key: Vec<u8>, value: Vec<u8>) -> Result<(), BackendError> {
-        self.put_cf(&key, &value)
+        let cf = self.cf_handle()?;
+        self.db
+            .put_cf(&cf, key, value)
+            .map_err(|e| BackendError::IoError(e.to_string()))
     }
 
     fn get_state(&self, key: Vec<u8>) -> Result<Option<Vec<u8>>, BackendError> {
-        self.get_cf(&key)
+        let cf = self.cf_handle()?;
+        self.db
+            .get_cf(&cf, key)
+            .map_err(|e| BackendError::IoError(e.to_string()))
     }
 
     fn delete_state(&self, key: Vec<u8>) -> Result<(), BackendError> {
-        self.delete_cf(&key)
+        let cf = self.cf_handle()?;
+        self.db
+            .delete_cf(&cf, key)
+            .map_err(|e| BackendError::IoError(e.to_string()))
     }
 
-    fn list_states(
-        &self,
-        start_inclusive: Vec<u8>,
-        end_exclusive: Vec<u8>,
-    ) -> Result<Vec<Vec<u8>>, BackendError> {
-        let mut keys = Vec::new();
+    fn list_states(&self, start: Vec<u8>, end: Vec<u8>) -> Result<Vec<Vec<u8>>, BackendError> {
+        let cf = self.cf_handle()?;
+        let mut ropts = ReadOptions::default();
+        ropts.set_iterate_upper_bound(end.clone());
 
-        let iter = if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            self.db.iterator_cf(
-                &cf,
-                IteratorMode::From(&start_inclusive, rocksdb::Direction::Forward),
-            )
-        } else {
-            self.db.iterator(IteratorMode::From(
-                &start_inclusive,
-                rocksdb::Direction::Forward,
-            ))
-        };
+        let iter =
+            self.db
+                .iterator_cf_opt(&cf, ropts, IteratorMode::From(&start, Direction::Forward));
+        let mut results = Vec::with_capacity(1024);
 
         for item in iter {
-            let (key, _) =
-                item.map_err(|e| BackendError::IoError(format!("RocksDB iterator error: {}", e)))?;
-            let key_slice = key.as_ref();
-
-            if key_slice >= start_inclusive.as_slice() && key_slice < end_exclusive.as_slice() {
-                keys.push(key.to_vec());
-            } else if key_slice >= end_exclusive.as_slice() {
+            let (k, _) = item.map_err(|e| BackendError::IoError(e.to_string()))?;
+            if k.as_ref() >= end.as_slice() {
                 break;
             }
+            results.push(k.to_vec());
         }
-
-        Ok(keys)
+        Ok(results)
     }
 
     fn merge(
@@ -238,181 +136,116 @@ impl StateStore for RocksDBStateStore {
         user_key: Vec<u8>,
         value: Vec<u8>,
     ) -> Result<(), BackendError> {
-        let key_bytes = build_key(&key_group, &key, &namespace, &user_key);
-        self.merge_cf(&key_bytes, &value)
+        let cf = self.cf_handle()?;
+        let full_key = build_key(&key_group, &key, &namespace, &user_key);
+        self.db
+            .merge_cf(&cf, full_key, value)
+            .map_err(|e| BackendError::IoError(e.to_string()))
     }
 
     fn delete_prefix_bytes(&self, prefix: Vec<u8>) -> Result<usize, BackendError> {
         if prefix.is_empty() {
-            return Err(BackendError::Other("Empty prefix not allowed".to_string()));
+            return Err(BackendError::Other("Empty prefix".into()));
+        }
+        let cf = self.cf_handle()?;
+
+        if !is_all_0xff(&prefix) {
+            let end_key = increment_key(&prefix);
+            self.db
+                .delete_range_cf(&cf, &prefix, &end_key)
+                .map_err(|e| BackendError::IoError(e.to_string()))?;
+            return Ok(0);
         }
 
-        if is_all_0xff(&prefix) {
-            let iter = if let Some(ref cf_name) = self.column_family_name {
-                let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                    BackendError::Other(format!("Column family '{}' not found", cf_name))
-                })?;
-                self.db.iterator_cf(
-                    &cf,
-                    IteratorMode::From(&prefix, rocksdb::Direction::Forward),
-                )
-            } else {
+        let mut batch = WriteBatch::default();
+        let mut count = 0;
+        let iter = self.db.prefix_iterator_cf(&cf, &prefix);
+
+        for item in iter {
+            let (k, _) = item.map_err(|e| BackendError::IoError(e.to_string()))?;
+            if !k.starts_with(&prefix) {
+                break;
+            }
+            batch.delete_cf(&cf, k);
+            count += 1;
+            if count % 1000 == 0 {
                 self.db
-                    .iterator(IteratorMode::From(&prefix, rocksdb::Direction::Forward))
-            };
-
-            let mut batch = WriteBatch::default();
-            let mut count = 0;
-
-            for item in iter {
-                let (key, _) = item
-                    .map_err(|e| BackendError::IoError(format!("RocksDB iterator error: {}", e)))?;
-                let key_slice = key.as_ref();
-
-                if key_slice.starts_with(&prefix) {
-                    if let Some(ref cf_name) = self.column_family_name {
-                        let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                            BackendError::Other(format!("Column family '{}' not found", cf_name))
-                        })?;
-                        batch.delete_cf(&cf, key_slice);
-                    } else {
-                        batch.delete(key_slice);
-                    }
-                    count += 1;
-                } else {
-                    break;
-                }
+                    .write(batch)
+                    .map_err(|e| BackendError::IoError(e.to_string()))?;
+                batch = WriteBatch::default();
             }
-
-            if count > 0 {
-                self.db.write(batch).map_err(|e| {
-                    BackendError::IoError(format!("RocksDB batch write error: {}", e))
-                })?;
-            }
-
-            return Ok(count);
         }
-
-        let end_key = increment_key(&prefix);
-        self.delete_range_cf(&prefix, &end_key)?;
-
-        Ok(0)
+        self.db
+            .write(batch)
+            .map_err(|e| BackendError::IoError(e.to_string()))?;
+        Ok(count)
     }
 
     fn scan(&self, prefix: Vec<u8>) -> Result<Box<dyn StateIterator>, BackendError> {
-        Ok(Box::new(RocksDBStateIterator {
-            db: self.db.clone(),
-            column_family_name: self.column_family_name.clone(),
+        Ok(Box::new(RocksDBStateIterator::new(
+            self.db.clone(),
+            self.cf_name.clone(),
             prefix,
-            current: Arc::new(Mutex::new(None)),
-            last_key: Arc::new(Mutex::new(None)),
-        }))
+        )?))
     }
 }
 
-/// RocksDB state iterator
-///
-/// Uses RocksDB's native iterator directly without loading all data into memory
-/// Uses internal state to track current iteration position
-struct RocksDBStateIterator {
-    /// RocksDB database instance
-    db: Arc<DB>,
-    /// Column family name (if using column family)
-    column_family_name: Option<String>,
-    /// Prefix (for filtering)
-    prefix: Vec<u8>,
-    /// Currently cached key-value pair (for has_next check)
-    current: Arc<Mutex<Option<(Vec<u8>, Vec<u8>)>>>,
-    /// Last accessed key (for continuing from last position)
-    last_key: Arc<Mutex<Option<Vec<u8>>>>,
+pub struct RocksDBStateIterator {
+    _db: Arc<DB>,
+    buffer: std::vec::IntoIter<(Vec<u8>, Vec<u8>)>,
 }
 
 impl RocksDBStateIterator {
-    /// Find the next matching item
-    fn find_next(&self) -> Result<Option<(Vec<u8>, Vec<u8>)>, BackendError> {
-        let last_key = self
-            .last_key
-            .lock()
-            .map_err(|e| BackendError::Other(format!("Lock error: {}", e)))?;
+    fn new(db: Arc<DB>, cf_name: String, prefix: Vec<u8>) -> Result<Self, BackendError> {
+        let mut collected = Vec::with_capacity(512);
+        {
+            let cf = db
+                .cf_handle(&cf_name)
+                .ok_or_else(|| BackendError::Other("CF missing".into()))?;
+            let mut ropts = ReadOptions::default();
+            ropts.set_prefix_same_as_start(true);
 
-        let start_key = if let Some(ref last) = *last_key {
-            IteratorMode::From(last.as_slice(), rocksdb::Direction::Forward)
-        } else {
-            IteratorMode::From(&self.prefix, rocksdb::Direction::Forward)
-        };
+            let iter =
+                db.iterator_cf_opt(&cf, ropts, IteratorMode::From(&prefix, Direction::Forward));
 
-        let iter = if let Some(ref cf_name) = self.column_family_name {
-            let cf = self.db.cf_handle(cf_name).ok_or_else(|| {
-                BackendError::Other(format!("Column family '{}' not found", cf_name))
-            })?;
-            self.db.iterator_cf(&cf, start_key)
-        } else {
-            self.db.iterator(start_key)
-        };
-
-        for item in iter {
-            let (key, value) =
-                item.map_err(|e| BackendError::IoError(format!("RocksDB iterator error: {}", e)))?;
-            let key_slice = key.as_ref();
-
-            if let Some(ref last) = *last_key
-                && key_slice <= last.as_slice()
-            {
-                continue;
-            }
-
-            if key_slice.starts_with(&self.prefix) {
-                drop(last_key);
-                let mut last_key = self
-                    .last_key
-                    .lock()
-                    .map_err(|e| BackendError::Other(format!("Lock error: {}", e)))?;
-                *last_key = Some(key.to_vec());
-                return Ok(Some((key.to_vec(), value.to_vec())));
-            } else if key_slice > self.prefix.as_slice() {
-                break;
+            for item in iter {
+                let (k, v) = item.map_err(|e| BackendError::IoError(e.to_string()))?;
+                if !k.starts_with(&prefix) {
+                    break;
+                }
+                collected.push((k.to_vec(), v.to_vec()));
             }
         }
 
-        Ok(None)
+        Ok(Self {
+            _db: db,
+            buffer: collected.into_iter(),
+        })
     }
 }
 
 impl StateIterator for RocksDBStateIterator {
     fn has_next(&mut self) -> Result<bool, BackendError> {
-        {
-            let current = self
-                .current
-                .lock()
-                .map_err(|e| BackendError::Other(format!("Lock error: {}", e)))?;
-            if current.is_some() {
-                return Ok(true);
-            }
-        }
-
-        if let Some(pair) = self.find_next()? {
-            let mut current = self
-                .current
-                .lock()
-                .map_err(|e| BackendError::Other(format!("Lock error: {}", e)))?;
-            *current = Some(pair);
-            Ok(true)
-        } else {
-            Ok(false)
-        }
+        Ok(self.buffer.as_slice().len() > 0)
     }
 
     fn next(&mut self) -> Result<Option<(Vec<u8>, Vec<u8>)>, BackendError> {
-        {
-            let mut current = self
-                .current
-                .lock()
-                .map_err(|e| BackendError::Other(format!("Lock error: {}", e)))?;
-            if let Some(pair) = current.take() {
-                return Ok(Some(pair));
-            }
-        }
-
-        self.find_next()
+        Ok(self.buffer.next())
     }
+}
+
+fn merge_operator(
+    _: &[u8],
+    existing: Option<&[u8]>,
+    operands: &rocksdb::MergeOperands,
+) -> Option<Vec<u8>> {
+    let size = existing.map_or(0, |v| v.len()) + operands.iter().map(|o| o.len()).sum::<usize>();
+    let mut buf = Vec::with_capacity(size);
+    if let Some(v) = existing {
+        buf.extend_from_slice(v);
+    }
+    for op in operands {
+        buf.extend_from_slice(op);
+    }
+    Some(buf)
 }
